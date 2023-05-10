75f560bafde7e5e13a615429fe4123bdd6a79c92
81809 <a81809@MacBook-Pro-33.local>
2023-05-10 13:53:52 +0900

    initial commit

---
+++ DAY1/todoapp/src/model/TodoItemModel.js
@@ -0,0 +1,20 @@
+let todoIdx = 0;
+
+export class TodoItemModel {
+    /** @type {number} TodoアイテムのID */
+    id;
+    /** @type {string} Todoアイテムのタイトル */
+    title;
+    /** @type {boolean} Todoアイテムが完了済みならばtrue、そうでない場合はfalse */
+    completed;
+
+    /**
+     * @param {{ title: string, completed: boolean }}
+     */
+    constructor({ title, completed }) {
+        // idは連番となり、それぞれのインスタンス毎に異なるものとする
+        this.id = todoIdx++;
+        this.title = title;
+        this.completed = completed;
+    }
+}
